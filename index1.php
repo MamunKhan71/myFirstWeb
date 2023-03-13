006d19c60b7abc9a326a24e5180db6ebef2a2926
Md. Mamun <82976724+MamunKhan71@users.noreply.github.com>
2023-03-13 23:58:09 +0600

    Update index1.php

--- index1.php
+++ index1.php
@@ -1,14 +1,1 @@
-<?php 
-    define("TITLE","Home | Mamun's Daining!");
-    include('includes/header.php');
-    
-?>
-    <div id="philosophy">
-    <hr>
-        <h1> Mamun's Dine</h1>
-        <p>Here at Mamun's dine, we know that good food isn't just about how expensive the dish is. We're not pompous, we're proud. We're proud of our work, our quality, our environment, and our love for food and family.</p>
-		<p><b>Oh, it's a very pleasure to meet you. You're family here.</b></p>
-    </div>
-<?php 
-    include('includes/footer.php');
-?>
+
